264ba993b2bcc4c7c584a48b8a0c7f23beb3444a
Keith Allatt <keith.allatt@gmail.com>
2019-10-16 10:20:16 -0400

    Added file and folder creation

--- note.sh
+++ note.sh
@@ -1,6 +1,6 @@
 # note 
 
-# Different ways of using note
+# Usage
 
 ## CASE 1
 # > note -f [Folder name]
@@ -19,11 +19,29 @@
 #> note -r [Folder name] [Note name]
 #	Remove the specified note
 
-
 ## CASE 5
 # > note [Folder name] [Note name]
 #	Create a note with the above name in the above folder. 
 # 	If the folder does not exist, raise an error.
+#	If no folder name is supplied, a 'default' folder is created / used
+
+######
+
+notes_folder=~/Documents/.notes/
+default_notes=~/Documents/.notes/default/
+
+mkdir -pv "$notes_folder"
+mkdir -pv "$default_notes"
+
+#####
+
+if [ "$#" -eq "0" ]
+then
+	echo "Sourcing default folders"
+	exit 0
+fi
+
+#####
 
 r_flag=false
 f_flag=false
@@ -33,20 +51,32 @@ print_conflicting() {
 }
 
 print_usage() {
-  printf "Usage: note [-r] [-f] [folder] [notename] \n"
+  printf "Usage: note [-r | -f] [folder] [notename] \n\t-r | Remove a folder or file\n\t-f | Create folder\n"
 }
 
 while getopts 'rf' flag; do
   case "${flag}" in
     r) r_flag=true ;;
     f) f_flag=true ;;
-    *) print_usage
+    \?) print_usage
        exit 1 ;;
   esac
 done
 
-if [ r_flag -a f_flag ] 
+if [ r_flag = true ]
 then
+	echo "R flag set"
+fi
+if [ f_flag = true ]
+then
+	echo "F flag set"
+fi
+
+
+if [ r_flag = true -a f_flag = true ] 
+then
+	echo "This is the error place\n"
+	
 	print_conflicting ;
 	print_usage ;
 	exit 1 ;
@@ -62,13 +92,39 @@ then
 	then
 		# CASE 1
 		
+		# find out if the user put the flag in the right place:
+		if [ "$1" = "-f" ]
+		then
+			# first argument is -f
+			# make directory on second arg
+			cd "$notes_folder"
+			mkdir -pv "$2"
+			cd ~
+		else
+			print_usage
+			exit 1
+		fi
+
 		exit 0;
 	fi
 
 	if [ "$#" -eq "3" ]
 	then
 		# CASE 2
-
+		
+		# find out if the user put the flag in the right place:
+		if [ "$1" = "-f" ]
+		then
+			# first argument if -f
+			# make directory on second arg
+			cd "$notes_folder"
+			mkdir -pv "$2"
+			# create file in that directory
+			cd "$2"
+			touch "$3" 
+			# open said file in Sublime
+			open -a "Sublime Text" "$3"
+		fi
 		exit 0;
 	fi
 	print_usage ;
@@ -100,4 +156,15 @@ fi
 # create a note ( no flags passed )
 # CASE 5
 
+if [ "$#" -eq "2" ]
+then
+	# Create a file
+	
+	
+
+	exit 0;
+fi
+
+print_usage ;
+exit 1 ;
 
